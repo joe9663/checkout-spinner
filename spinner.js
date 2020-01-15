fbf9c21578f25f38828df8cec09d173d4e3ef2dc
joe9663 <joe9663@gmail.com>
2020-01-15 15:28:28 -0500

    adds localize and fixes jquery event management

--- spinner.js
+++ spinner.js
@@ -1,7 +1,10 @@
-jQuery(document).ready(function(){
-    jQuery('#place_order').click(function () {
-       jQuery('#place_order').attr('disabled', 'disabled');
-       jQuery('#place_order').replaceWith('<img style="margin:0 auto;" src="/wp-admin/images/spinner-2x.gif">')
-       jQuery('.checkout').submit();
+jQuery(document).ready(function($){
+    $('body').on('click', '#place_order', function () {
+       $('#place_order').attr('disabled', 'disabled');
+       (wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
+       spinner.
+       $('#place_order').replaceWith('<img id="spinner" style="margin:0 auto;">');
+       $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif")
+       $('.checkout').submit();
      });
 });
