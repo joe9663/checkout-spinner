2f5ee2df6fde4728317a19d25b8815ab2b741b02
joe9663 <joe9663@gmail.com>
2020-01-15 15:30:06 -0500

    removes debugging code in script

--- spinner.js
+++ spinner.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function($){
     $('body').on('click', '#place_order', function () {
        $('#place_order').attr('disabled', 'disabled');
-       (wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
-       spinner.
        $('#place_order').replaceWith('<img id="spinner" style="margin:0 auto;">');
-       $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif")
+       $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
        $('.checkout').submit();
      });
 });
