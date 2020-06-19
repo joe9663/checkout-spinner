bec79d83c7fb17648a02083424d89a7693532878
joe9663 <joe9663@gmail.com>
2020-06-19 10:47:18 -0400

    adds check for valid cc info and terms acceptance before adding spinner

--- spinner.js
+++ spinner.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function($){
     $('body').on('click', '#place_order', function () {
-       $('#place_order').attr('disabled', 'disabled');
-       $('#place_order').replaceWith('<img id="spinner" style="margin:0 auto;">');
-       $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
-       $('.checkout').submit();
+        if (($('#terms').is(':checked'))  && ($('#stripe-card-element').hasClass('StripeElement--complete'))){
+            $('#place_order').attr('disabled', 'disabled');
+            $('#place_order').replaceWith('<img id="spinner" style="margin:0 auto;">');
+            $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
+            $('.checkout').submit();
+        }
      });
 });
