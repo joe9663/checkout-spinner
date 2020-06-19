0ff309637a64ab2ee063434e8ecd3c107a0769ed
joe9663 <joe9663@gmail.com>
2020-06-19 10:51:41 -0400

    fixes typo in validation

--- spinner.js
+++ spinner.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
     $('body').on('click', '#place_order', function () {
-        if (($('#terms').is(':checked'))  && ($('#stripe-card-element').hasClass('StripeElement--complete'))){
+        if (($('#terms').is(':checked')) && ($('#stripe-card-element').hasClass('StripeElement--complete'))){
             $('#place_order').attr('disabled', 'disabled');
             $('#place_order').replaceWith('<img id="spinner" style="margin:0 auto;">');
             $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
