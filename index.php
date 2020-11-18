6fd11b7572f3e098ff1e1bbb452a62de1cc28ff3
joe9663 <joe9663@gmail.com>
2020-11-18 12:56:04 -0500

    fixes validation check errors

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Checkout Spinner
  * Description: Changes Checkout button to spinner on form submission.
- * Version: 1.1
+ * Version: 1.1.1
  * Plugin URI: https://github.com/joe9663/checkout-spinner
  * Author: Joseph Wood & Harry Jackson
  * Author URI: https://github.com/joe9663
@@ -12,9 +12,8 @@
  *
  * @since 1.0.0
  */
-function checkout_spinner()
-{
+function checkout_spinner() {
     wp_enqueue_script( 'wc_checkout_spinner' , plugin_dir_url( __FILE__ ) . 'spinner.js', array('jquery'), false , true );
     wp_localize_script( 'wc_checkout_spinner', 'wpAjax', array( 'wpurl' => get_bloginfo('wpurl') ) );
 }
-add_action('woocommerce_after_checkout_validation', 'checkout_spinner');
+add_action('wp_enqueue_scripts', 'checkout_spinner');
--- spinner.js
+++ spinner.js
@@ -1,10 +1,16 @@
 jQuery(document).ready(function($){
     $('body').on('click', '#place_order', function () {
-        if (($('#terms').is(':checked')) && ($('#stripe-card-element').hasClass('StripeElement--complete'))){
-            $('#place_order').attr('disabled', 'disabled');
-            $('#place_order').replaceWith('<img id="spinner" style="margin:0 auto;">');
-            $('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
-            $('.checkout').submit();
-        }
+		$('.woocommerce-error li').remove();
+        setTimeout( 
+			function() {
+				if ($('.woocommerce-error li').length == 0 ) {
+            		$('#place_order').attr('disabled', 'disabled');
+            		$('#place_order').replaceWith('<img id="spinner" style="margin:0 auto; display:flex;">');
+            		$('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
+            		$('.checkout').submit();
+        		}
+			}, 500
+		);
+        
      });
 });
