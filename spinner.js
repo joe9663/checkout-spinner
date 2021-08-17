a0773d0e3e39f2e790c3be6cf2945cd180eed963
joe9663 <joe9663@gmail.com>
2021-08-17 11:56:19 -0400

    adding stuff

--- spinner.js
+++ spinner.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function($){
     $('body').on('click', '#place_order', function () {
-		$('.woocommerce-error li').remove();
-        setTimeout( 
-			function() {
-				if ($('.woocommerce-error li').length == 0 ) {
+	//	$('.woocommerce-error li').remove();
+        //setTimeout( 
+		//	function() {
+				if ( ($('.woocommerce-invalid').length == 0 ) && ($('#terms').is(':checked')) && ($('.woocommerce-error').length == 0)) {
             		$('#place_order').attr('disabled', 'disabled');
             		$('#place_order').replaceWith('<img id="spinner" style="margin:0 auto; display:flex;">');
             		$('#spinner').attr('src', wpAjax.wpurl + "/wp-admin/images/spinner-2x.gif");
             		$('.checkout').submit();
         		}
-			}, 500
-		);
+		//	}, 500
+    //);
         
      });
 });
